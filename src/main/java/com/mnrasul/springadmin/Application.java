5232d31c305180ca7fbf5a7eab814b6e48810688
Nasir Rasul <nasir.rasul@promontech.com>
2017-11-28 14:47:49 -0700

    removed config

--- src/main/java/com/mnrasul/springadmin/Application.java
+++ src/main/java/com/mnrasul/springadmin/Application.java
@@ -1,13 +1,10 @@
 package com.mnrasul.springadmin;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-/**
- * @author Nasir Rasul {@literal nasir@mnrasul.com}
- */
+import de.codecentric.boot.admin.config.EnableAdminServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
